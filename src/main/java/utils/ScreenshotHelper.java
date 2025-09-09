f37f2349c0576ec839b66f9c11d911bd9d2e9944
Anthony9811 <ajortega2290@gmail.com>
2025-09-09 14:45:35 -0600

    refactor: Improve screenshot file naming
    Refactors the screenshot utility to dynamically name the output files after the test method that generated them. This improvement makes it much easier to identify the cause of a failure and organizes the screenshot directory for better test reporting.

--- src/main/java/utils/ScreenshotHelper.java
+++ src/main/java/utils/ScreenshotHelper.java
@@ -7,25 +7,36 @@ import org.openqa.selenium.WebDriver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ScreenshotHelper {
 
+    private static final String SCREENSHOTS_DIRECTORY = System.getProperty("user.dir") + "/test-output/screenshots/";
+
     public static String takeAScreenshot(WebDriver driver, String testName) {
-        String timestamp = new SimpleDateFormat("dd MMMM yyyy").format(new Date());
-        String filePath = System.getProperty("user.dir") + "test-output/screenshots/" +
-                                                            testName + "_" + timestamp + ".png";
-        File screenshotSource = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        File destination = new File(filePath);
+        Path testFolder = Path.of(SCREENSHOTS_DIRECTORY, testName);
 
         try {
-            Files.createDirectories((destination.getParentFile().toPath()));
-            Files.copy(screenshotSource.toPath(), destination.toPath());
+            Files.createDirectories(testFolder);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to create screenshot folder: " + testFolder, e);
         }
 
-        return filePath;
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS").format(new Date());
+        String fileName = testName + "_" + timestamp + ".png";
+
+        Path destination = testFolder.resolve(fileName);
+
+        try {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            Files.copy(screenshot.toPath(), destination);
+
+            return destination.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save screenshot: " + destination, e);
+        }
     }
 }
+
